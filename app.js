62bb265580fc04e6de27dc90262e8ea4509d313f
Steren Giannini <steren.giannini@gmail.com>
2012-07-23 17:15:21 +0200

    bugfix: hostname

--- app.js
+++ app.js
@@ -122,10 +122,12 @@ app.set('view options', {routes: routes});
  */
 app.get('/*', function (req, res, next){
   var context = null;
-  var referrer = req.header('Host');
+  var host = req.header('Host');
 
-  if(referrer.indexOf("docs") > 0) { context = "docs"; }
-  else if(referrer.indexOf("developer") > 0) { context = "developer"; }
+  console.log(host);
+
+  if(host.indexOf("docs") > -1) { context = "docs"; }
+  else if(host.indexOf("developer") > -1) { context = "developer"; }
 
   res.local("context", context);
   next();
